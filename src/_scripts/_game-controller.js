2da2912adbd18b6e7e30ffd349d21c1e7b322ac9
Benjamin Richardson <benjam.richardson@gmail.com>
2017-11-02 16:10:02 +0800

    Reorganise code for better re-use and new purpose of the game

--- src/_scripts/_game-controller.js
+++ src/_scripts/_game-controller.js
@@ -9,9 +9,46 @@ import Card from 'card/card';
 export default class GameController {
   constructor() {
     let _self = this;
+    const API_START = 'classes';
 
     this.cardClass = new Card();
+    this.getTemplates();
 
+    window.dnd.emitter.on('dnd-game-start', function(){
+      // Set random class
+      const TOTAL_DND_CLASSES = 12; // Just going to code this value as it donesnt change much
+      const CLASS_NUMBER = _self.getRandomInt(1, TOTAL_DND_CLASSES);
+
+      let randomClassUrl = `${_self.API_ENDPOINT}${API_START}/${CLASS_NUMBER}`;
+      _self.getShowClass(randomClassUrl);
+
+    });
+
+    window.dnd.emitter.on('dnd-choice-made', function(choiceId){
+
+      let dndClassUrl = `${_self.API_ENDPOINT}${API_START}/${choiceId}`;
+      _self.getShowClass(dndClassUrl);
+
+    });
+  }
+
+  getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+  }
+
+  getSettings(apiUrl) {
+    return {
+      url: apiUrl,
+      type: 'GET',
+      cache: true,
+      timeout: 10000,
+      dataType: 'json'
+    };
+  }
+
+  getTemplates() {
     // Card template
     this.classCardTemplate;
 
@@ -36,105 +73,80 @@ export default class GameController {
       console.error('Error: API endpoint not defined');
       return;
     }
+  }
 
-    const API_START = 'classes';
+  getShowClass(classUrl) {
+    let _self = this;
+    // Call API for class content
+    $.ajax(_self.getSettings(classUrl)).done((data) => {
+      console.log(data);
 
-    // Set random class
-    const TOTAL_DND_CLASSES = 12; // Just going to code this value as it donesnt change much
-    const CLASS_NUMBER = this.getRandomInt(1, TOTAL_DND_CLASSES);
+      // Transition in game containers
+      $('.game-class').show();
+      $('.game-class__welcome').fadeIn('800');
 
-    window.dnd.emitter.on('dnd-game-start', function(){
+      // Set up containers requiring additional API calls first
+      let classCardHtml = _self.classCardTemplate(data);
 
-      let randomClassUrl = `${_self.API_ENDPOINT}${API_START}/${CLASS_NUMBER}`;
+      // Display first round of content
+      $('.game-class__container').html(classCardHtml);
+      $('.game-loader').css('opacity', '0');
 
-      // Call API for class content
-      $.ajax(_self.getSettings(randomClassUrl)).done((data) => {
-        console.log(data);
+      // Call off for additional second level items
+      let subclassApi = $('.subclass').data('api-endpoint') || '';
+      $.ajax(_self.getSettings(subclassApi)).done((data) => {
+        // Display second round content
+        console.log('subclassApi', data);
 
-        // Transition in game containers
-        $('.game-class').show();
-        $('.game-class__welcome').fadeIn('800');
+        $('.subclass').text(data.desc[0]);
+      });
 
-        // Set up containers requiring additional API calls first
-        let classCardHtml = _self.classCardTemplate(data);
+      let startingEquipmentApi = $('.starting-equipment').data('api-endpoint') || '';
+      $.ajax(_self.getSettings(startingEquipmentApi)).done((data) => {
+        // Display second round content
+        console.log('startingEquipmentApi', data);
 
-        // Display first round of content
-        $('.game-class__container').html(classCardHtml);
-        $('.game-loader').css('opacity', '0');
-
-        // Call off for additional second level items
-        let subclassApi = $('.subclass').data('api-endpoint') || '';
-        $.ajax(_self.getSettings(subclassApi)).done((data) => {
-          // Display second round content
-          console.log('subclassApi', data);
-
-          $('.subclass').text(data.desc[0]);
+        // Add in default starting items
+        let defaultStartingEquipmentHtml = '';
+        data.starting_equipment.forEach(element => {
+          defaultStartingEquipmentHtml += `<div class="chip">${element.item.name} x ${element.quantity}</div>`;
         });
 
-        let startingEquipmentApi = $('.starting-equipment').data('api-endpoint') || '';
-        $.ajax(_self.getSettings(startingEquipmentApi)).done((data) => {
-          // Display second round content
-          console.log('startingEquipmentApi', data);
-
-          // Add in default starting items
-          let defaultStartingEquipmentHtml = '';
-          data.starting_equipment.forEach(element => {
-            defaultStartingEquipmentHtml += `<div class="chip">${element.item.name} x ${element.quantity}</div>`;
-          });
-
-          $('.starting-equipment').prepend(defaultStartingEquipmentHtml);
+        $('.starting-equipment').prepend(defaultStartingEquipmentHtml);
 
 
-          let startingEquipmentChoices = '',
-            choiceInitValues = [],
-            dropDownData = [];
+        let startingEquipmentChoices = '',
+          choiceInitValues = [],
+          dropDownData = [];
 
-          // Add in choices
-          for(let i = 0; i <= data.choices_to_make; i++) {
-            let indexString = String(i+1);
-            if(data[`choice_${indexString}`] !== undefined) {
-              startingEquipmentChoices += _self.classCardSelectTemplate({'index': indexString});
+        // Add in choices
+        for(let i = 0; i <= data.choices_to_make; i++) {
+          let indexString = String(i+1);
+          if(data[`choice_${indexString}`] !== undefined) {
+            startingEquipmentChoices += _self.classCardSelectTemplate({'index': indexString});
 
-              dropDownData = data[`choice_${indexString}`].reduce(function(concatArrays, choices) {
-                return choices.from.concat(concatArrays);
-              },[]);
-            }
-
-            choiceInitValues.push(dropDownData);
+            dropDownData = data[`choice_${indexString}`].reduce(function(concatArrays, choices) {
+              return choices.from.concat(concatArrays);
+            },[]);
           }
 
-          // Set up select and initialise
-          $('.starting-equipment .starting-equipment__selects').html(startingEquipmentChoices);
-          choiceInitValues.forEach(function(equipment, index) {
-            let dropDownDataFormatted = {};
+          choiceInitValues.push(dropDownData);
+        }
 
-            equipment.forEach(function(item){
-              dropDownDataFormatted[item.item.name] = null;
-            });
+        // Set up select and initialise
+        $('.starting-equipment .starting-equipment__selects').html(startingEquipmentChoices);
+        choiceInitValues.forEach(function(equipment, index) {
+          let dropDownDataFormatted = {};
 
-            _self.cardClass.initialiseInput($('#starting-item-input-' + (index + 1)), dropDownDataFormatted);
+          equipment.forEach(function(item){
+            dropDownDataFormatted[item.item.name] = null;
           });
 
+          _self.cardClass.initialiseInput($('#starting-item-input-' + (index + 1)), dropDownDataFormatted);
         });
 
       });
 
     });
-  }
-
-  getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
-  }
-
-  getSettings(apiUrl) {
-    return {
-      url: apiUrl,
-      type: 'GET',
-      cache: true,
-      timeout: 10000,
-      dataType: 'json'
-    };
   }
 }
